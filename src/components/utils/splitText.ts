8c59bd6ed7a0ccf61c77a5ace4664368bb3b7732
zohaib-ds <ds.zohaibzeeshan@gmail.com>
2026-03-19 21:35:03 +0500

    splitText Updated

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -13,6 +13,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
   if (window.innerWidth < 900) return;
+
   const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
   const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
 
@@ -32,7 +33,7 @@ export default function setSplitText() {
     });
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      para.split?.words ?? [], // ✅ added optional chaining and default empty array
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
@@ -48,6 +49,7 @@ export default function setSplitText() {
       }
     );
   });
+
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
@@ -57,8 +59,9 @@ export default function setSplitText() {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
     title.anim = gsap.fromTo(
-      title.split.chars,
+      title.split?.chars ?? [], // ✅ optional chaining + default array
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
